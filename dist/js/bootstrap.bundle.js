7b4e521f70800cc1321815b6542f82b7dd27d91c
Perseus The Great <arash.laylazi@live.com>
2018-09-11 00:26:12 +0430

    Dist ver. 4.1.3, RTL rev. 8

--- dist/js/bootstrap.bundle.js
+++ dist/js/bootstrap.bundle.js
@@ -1156,7 +1156,11 @@
 
         if (this._parent) {
           actives = [].slice.call(this._parent.querySelectorAll(Selector.ACTIVES)).filter(function (elem) {
-            return elem.getAttribute('data-parent') === _this._config.parent;
+            if (typeof _this._config.parent === 'string') {
+              return elem.getAttribute('data-parent') === _this._config.parent;
+            }
+
+            return elem.classList.contains(ClassName.COLLAPSE);
           });
 
           if (actives.length === 0) {
@@ -1293,7 +1297,7 @@
       _proto._getParent = function _getParent() {
         var _this3 = this;
 
-        var parent = null;
+        var parent;
 
         if (Util.isElement(this._config.parent)) {
           parent = this._config.parent; // It's a jQuery object
@@ -1314,12 +1318,10 @@
       };
 
       _proto._addAriaAndCollapsedClass = function _addAriaAndCollapsedClass(element, triggerArray) {
-        if (element) {
-          var isOpen = $$$1(element).hasClass(ClassName.SHOW);
+        var isOpen = $$$1(element).hasClass(ClassName.SHOW);
 
-          if (triggerArray.length) {
-            $$$1(triggerArray).toggleClass(ClassName.COLLAPSED, !isOpen).attr('aria-expanded', isOpen);
-          }
+        if (triggerArray.length) {
+          $$$1(triggerArray).toggleClass(ClassName.COLLAPSED, !isOpen).attr('aria-expanded', isOpen);
         }
       }; // Static
 
